39328e3920b9552848593e41599c65729d4c83ed
Agatha Bahati <agatha@marketforce.com>
2023-03-07 22:35:12 +0300

    feat: add endpoint to delete recipe

--- app/routes.py
+++ app/routes.py
@@ -35,8 +35,18 @@ def get_recipes(
 
 @app.get('/recipe/{recipe_id}', response_model=RecipeSerializer)
 def get_recipe(
-    recipe_id: str,
+    recipe_id: int,
     db: Session = Depends(get_db),
 ):
     recipe=db.query(Recipe).where(id==recipe_id).first()
     return recipe
+
+@app.delete('/recipe/{recipe_id}')
+def delete_recipe(
+    recipe_id: int,
+    db: Session = Depends(get_db)
+):
+    db_obj=db.query(Recipe).where(id==recipe_id).first()
+    db.delete(db_obj)
+    db.commit()
+    return {'message':'Recipe deleted'}
