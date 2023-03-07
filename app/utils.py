813bb42f95b463cddb938525bcbdd67a4a2091cd
Agatha Bahati <agatha@marketforce.com>
2023-03-07 23:12:47 +0300

    feat: add delete recipes util
    (cherry picked from commit 86b66f24589c630be9c3bf621a1e5def0582ed8d)

---
+++ app/utils.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import Session
+from schemas import RecipeSerializer
+from models import Recipe
+
+def create_recipe(db: Session,recipe: RecipeSerializer):
+    db_obj = Recipe(**recipe.dict())
+    db.add(db_obj)
+    db.commit()
+    db.refresh(db_obj)
+
+    return db_obj
+
+def get_recipe(db: Session, recipe_id:int):
+    recipe = db.query(Recipe).where(id==recipe_id).first()
+    return recipe
+
+def get_recipes(db: Session):
+    recipes = db.query(Recipe).all()
+    return recipes
+
+def delete_recipe(db: Session, recipe_id:int):
+    db_obj=db.query(Recipe).where(id==recipe_id).first()
+    db.delete(db_obj)
+    db.commit()
+    return {'message':'Recipe deleted'}
