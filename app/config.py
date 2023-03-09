9371769fab51795802e3ae3d3b60a0f8ec756be6
Agatha Bahati <agatha@marketforce.com>
2023-03-09 21:34:20 +0300

    feat: add config file

---
+++ app/config.py
@@ -0,0 +1,14 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    DB_PORT:int
+    POSTGRES_PASSWORD:str
+    POSTGRES_USER:str
+    POSTGRES_DB:str
+    POSTGRES_HOST:str
+    POSTGRES_HOSTNAME:str
+
+    class Config:
+        env_file = './.env'
+
+settings = Settings()
