2ea9703cec7b91c6d519aba6bab77fb75a40f30d
Agatha Bahati <agatha@marketforce.com>
2023-03-09 21:19:43 +0300

    feat: add postgresql and run through docker

--- app/db.py
+++ app/db.py
@@ -3,10 +3,10 @@ from sqlalchemy.orm import sessionmaker
 
 db_uri='sqlite+pysqlite:///./db.sqlite3:'
 engine=create_engine(db_uri, future=True, echo=True)
-session=sessionmaker(autocommit=False, autoflush=False,bind=engine)
+SessionLocal=sessionmaker(autocommit=False, autoflush=False,bind=engine)
 
 def get_db() -> None:
-    db=session()
+    db=SessionLocal()
     try:
         yield db
     finally:
