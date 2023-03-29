6d7ddbbf157389e8ea2a00a6b478d2604a218f74
Lewis Munyi <lewismunyiii@gmail.com>
2023-03-30 01:24:31 +0300

    Improvements: - Docker Networks - Redis Cache - Volumes
    Bugs:
    - API container won't start

--- app/config.py
+++ app/config.py
@@ -5,12 +5,10 @@ import os
 load_dotenv()
 
 class Settings(BaseSettings):
-    DB_PORT:int=os.getenv("DB_PORT")
+    POSTGRES_PORT:int=os.getenv("POSTGRES_PORT")
     POSTGRES_PASSWORD:str=os.getenv("POSTGRES_PASSWORD")
     POSTGRES_USER:str=os.getenv("POSTGRES_USER")
     POSTGRES_DB:str=os.getenv("POSTGRES_DB")
-    POSTGRES_HOST:str=os.getenv("POSTGRES_HOST")
     POSTGRES_HOSTNAME:str=os.getenv("POSTGRES_HOSTNAME")
 
-    
 settings = Settings()
