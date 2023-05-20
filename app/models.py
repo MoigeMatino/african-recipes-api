3b1ea1cd890724c5390c72e1198c6bb40913646e
Agatha Bahati <matinodev@gmail.com>
2023-05-20 13:31:42 +0300

    add migration
    fix

--- app/models.py
+++ app/models.py
@@ -117,6 +117,6 @@ class Image(Base):
     __tablename__ = "images"
 
     url: Mapped[str] = mapped_column(String, nullable=False)
-    user_id: Mapped[uuid.UUID] = mapped_column(String, ForeignKey("users.id"), nullable=False)
-    user = relationship(back_populates="images")
+    user_id: Mapped[uuid.UUID] = mapped_column(String, ForeignKey("user.id"), nullable=False)
+    user: Mapped["User"] = relationship(back_populates="image")
     
