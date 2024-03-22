0310c6cbe458ab0ed2806e120c0328c585351811
Lewis Munyi <lewismunyiii@gmail.com>
2024-03-22 17:50:38 +0300

    refactor: create ratings mcrs

---
+++ app/Models/Rating.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rating extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'recipe_id',
+        'user_id',
+        'rating',
+    ];
+}
