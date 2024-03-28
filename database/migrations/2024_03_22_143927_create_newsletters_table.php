9157935cd7a25ef663dae1d85df46f1ef5cf3f04
AgathaBahati <matinodev@gmail.com>
2024-03-28 07:51:18 +0300

    feat: Add soft delete for newsletters

--- database/migrations/2024_03_22_143927_create_newsletters_table.php
+++ database/migrations/2024_03_22_143927_create_newsletters_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('content')->nullable();
             $table->string('status')->default('draft');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
